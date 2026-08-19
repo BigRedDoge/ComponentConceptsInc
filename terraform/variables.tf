f0e71204fe9d068cb2fd40fe6e60b853860d340d
BigRedDoge <seancliff01@gmail.com>
2026-08-19 15:29:04 -0400

    oidc fix

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,7 +5,7 @@ variable "domain" {
 }
 
 variable "github_repo" {
-  description = "GitHub repository in org/repo format. Used to scope the OIDC deploy role trust policy."
+  description = "GitHub repository identifier for OIDC sub claims. Includes numeric org and repo IDs that GitHub appends to prevent sub-claim hijacking after renames."
   type        = string
-  default     = "BigRedDoge/ComponentConceptsInc"
+  default     = "BigRedDoge@23642952/ComponentConceptsInc@1339580913"
 }
